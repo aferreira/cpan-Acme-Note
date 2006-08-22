ebd6a540b8cc82f16aa293513b8978d13ac7b622
Adriano Ferreira <ferreira@cpan.org>
2006-08-22 17:18:03 +0000

    Import of FERREIRA/Acme-Note-0.2 from CPAN.
    gitpan-cpan-distribution: Acme-Note
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         FERREIRA/Acme-Note-0.2.tar.gz
    gitpan-cpan-author:       FERREIRA
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Note.pm
@@ -0,0 +1,70 @@
+package Acme::Note;
+
+use vars qw($VERSION);
+$VERSION = '0.2';
+
+=head1 NAME
+
+Acme::Note - Make a mental note for programming style
+
+=head1 SYNOPSIS
+
+  use Acme::Note;
+  # and then your honorable Perl script
+
+Or at command line
+
+  perl -MAcme::Note script.pl
+
+=head1 DESCRIPTION
+
+Sometimes you just have to remember that odd little thing.
+
+  Tell you what. Let's just issue a mandatory warning at the startup of
+  every Perl script that says:
+  "???"
+            --Larry Wall
+
+=head1 BUGS
+
+Besides being irritating? 
+
+File a bug at CPAN RT - via bug-Acme-Note@rt.cpan.org, 
+or through http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-Note.
+When I succeed to login, I will post a random reply.
+
+=head1 MAINTAINER
+
+I do not claim authorship of this. This module imposed to myself and I
+care for it while it doesn't bite me.
+
+Adriano R. Ferreira <ferreira@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 Adriano R. Ferreira, All Rights Reserved.
+
+The Acme::Note module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+# we don't play fair with test harnesses
+
+sub _skroook { local $_ = pop; s/[^zZ]//g; tr/zZ/01/; pack "b*", $_ }
+BEGIN { eval _skroook <<POONG;
+zZzZzzzzZZzzZZZzZzZzZZZzzZzzzZZzzzzzzZzzZzzZzZZzZzZZzZZzzzzz
+ZZZzZZZZzZZzzZzzZZZzzzZzZZZzzzzzzZzzZZzZZZZzzZzZzzzzzzzzzZzz
+zzzzzZzzZZZzZZZzZzzzzZZzzZzzZZZzzZZZzZZzzzzzzZzzzZzzzZzzzzZz
+zzZzZZZZzZZzzZZZzZZzZZZzzZzzzzZzZZZzzzzzzZzzzZzzzZZzZzZzzZZz
+zzzzzZzzZZzzZZZzzzZzZZZzZzZzZZZzzzzzZZZzZzzZzZZzzzZzzZZzzZZZ
+zZzzzzZZZzZzzZZZzZZzzZzzzZzzzZzZzzzzzzzzzZzzzzzzzZzzzzzzzZzz
+zzzzzZzzZzZzZZZzzZZZzZZzzzZZzZZzZzZzzZZzZZzzZZZzZZzzZZZzzzzz
+zZzzzzZzzZzzZzZzzzZzzZZZzzZzzZZzZzZzZZzZZZZzzzzZzzZzZzzzzzZz
+zZzzZzZzzZZZzzZzZzZzzzZzZZzzZzZzZZzzZzZzZZZZZzZzZzzzzzZzZZzz
+zzZzzzZzZzZzZzzZzzZzzZZzZzZzZzZzzzZzZzZZZZZzzZzZzzzzZzZZZZZz
+zZzZzzzzzZzZzzzz
+POONG
+} 
+
+1;
