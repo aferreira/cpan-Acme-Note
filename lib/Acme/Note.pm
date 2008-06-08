d45380b99e63d2d6bc92bf2bec074432e487e943
Adriano Ferreira <ferreira@cpan.org>
2008-06-08 17:42:26 +0000

    Import of FERREIRA/Acme-Note-0.7 from CPAN.
    gitpan-cpan-distribution: Acme-Note
    gitpan-cpan-version:      0.7
    gitpan-cpan-path:         FERREIRA/Acme-Note-0.7.tar.gz
    gitpan-cpan-author:       FERREIRA
    gitpan-cpan-maturity:     released

--- lib/Acme/Note.pm
+++ lib/Acme/Note.pm
@@ -4,54 +4,7 @@ use strict; # CPANTS made me do it!
 use warnings; # that too!
 
 use vars qw($VERSION);
-$VERSION = '0.6';
-
-=head1 NAME
-
-Acme::Note - Make a mental note for programming style
-
-=head1 SYNOPSIS
-
-  use Acme::Note;
-  # and then your honorable Perl script
-
-Or at command line
-
-  perl -MAcme::Note script.pl
-  perl -MAcme::Note=:colour script.pl
-
-=head1 DESCRIPTION
-
-Sometimes you just have to remember that odd little thing.
-
-  Tell you what. Let's just issue a mandatory warning at the startup of
-  every Perl script that says:
-  "???"
-            --Larry Wall
-
-=head1 BUGS
-
-Besides being irritating? 
-
-File a bug at CPAN RT - via bug-Acme-Note@rt.cpan.org, 
-or through http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-Note.
-When I succeed to login, I will post a random reply.
-
-=head1 MAINTAINER
-
-I do not claim authorship of this. This module imposed to myself and I
-care for it while it doesn't bite me.
-
-Adriano R. Ferreira <ferreira@cpan.org>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2006-2008 Adriano R. Ferreira
-
-The Acme::Note module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-=cut
+$VERSION = '0.7';
 
 sub _colored {
   require Term::ANSIColor;
